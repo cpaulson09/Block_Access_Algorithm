1a99e1fd73ddb69ebeab73991e98e76289651adb
Connor P <Z_10684469@cs3060.ad.uvu.edu>
2019-11-07 17:21:39 -0700

    uncommented getFileContents()

--- assn7.c
+++ assn7.c
@@ -1,36 +1,36 @@
-// #include <stdlib.h>
-// #include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-// //Program 5 CPU Scheduler
+//Program 7 Block Access Algorithms
 
-// #define LIMIT 20
+#define LIMIT 20
 
-// int blockList[LIMIT];
+int blockList[LIMIT];
 
-// void getFileContents(int argc, char file[]){
-// 	FILE *filePtr;
-// 	char *filename = file;
-// 	filePtr = fopen(filename, "r");
+void getFileContents(int argc, char file[]){
+	FILE *filePtr;
+	char *filename = file;
+	filePtr = fopen(filename, "r");
 
-// 	if(argc < 2){
-// 		printf("please give file name\n");
-// 	}
-// 	if (!filePtr){//if no file
-// 		printf("No File Contents\n");
-// 		//return 1;
-// 		exit(0);
-// 	}
+	if(argc < 2){
+		printf("please give file name\n");
+	}
+	if (!filePtr){//if no file
+		printf("No File Contents\n");
+		//return 1;
+		exit(0);
+	}
 
-// 	int a = 0;
-// 	int num;
-// 	//get each value from file
-// 	while(fscanf(filePtr, "%d", &num) > 0){
-//         blockList[a] = num;
-// 		a++;
-// 	}
+	int a = 0;
+	int num;
+	//get each value from file
+	while(fscanf(filePtr, "%d", &num) > 0){
+        blockList[a] = num;
+		a++;
+	}
 	
-// 	fclose(filePtr);
-// }
+	fclose(filePtr);
+}
 // void FCFS(int blocks[]){
 
 // 	int i = 0;
@@ -38,6 +38,7 @@
 // 	int add2Total = 0;
 
 // 	for (i = 0; i < LIMIT; i++){
+// 		if (blocks[i > ])
 // 		add2Total = blocks[i] - blocks[i+1];
 // 		combinedSeekTime = combinedSeekTime + add2Total;
 // 	}
