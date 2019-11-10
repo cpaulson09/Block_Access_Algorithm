0d8d260f6de8e2d7bbfec22e1cc9d7086c51f1f2
Connor P <Z_10684469@cs3060.ad.uvu.edu>
2019-11-10 13:56:35 -0700

    updated assn7.c

--- assn7.c
+++ assn7.c
@@ -34,27 +34,35 @@ void getFileContents(int argc, char file[]){
 // void FCFS(int blocks[]){
 
 // 	int i = 0;
-// 	int combinedSeekTime = 0;
+// 	int totalSeekAccumulator = 0;
 // 	int add2Total = 0;
 
 // 	for (i = 0; i < LIMIT; i++){
-// 		if (blocks[i] > )
-// 		add2Total = blocks[i] - blocks[i+1];
-// 		combinedSeekTime = combinedSeekTime + add2Total;
-// 	}
 
-// 	printf("FCFS Total Seek: %d\n", combinedSeekTime);
+// 		printf("Currently at: %d\n", blocks[i]);
+// 		if (blocks[i] > blocks[i+1]){
+// 			add2Total = blocks[i] - blocks[i+1];
+// 			// printf("add2total: %d\n", add2Total);  
+// 		}
+// 		else{
+// 			add2Total = blocks[i+1] - blocks[i];
+// 			// printf("add2total else: %d\n", add2Total);
+// 		}
+// 		totalSeekAccumulator = totalSeekAccumulator + add2Total;
+// 		add2Total = 0;
+// 	}
+// 	printf("\nFCFS Total Seek: %d\n", totalSeekAccumulator);
 // }
 
 int main(int argc, char* argv[]){
 
 	getFileContents(argc, argv[1]);
 
-    printf("\nblocks\n");
-	printf("------\n");
-	for (int i = 0; i < LIMIT; i++){
-		printf("%d\n", blockList[i]);
-	} 
+    // printf("\nblocks\n");
+	// printf("------\n");
+	// for (int i = 0; i < LIMIT; i++){
+	// 	printf("%d\n", blockList[i]);
+	// } 
 
 	FCFS(blockList);
 
